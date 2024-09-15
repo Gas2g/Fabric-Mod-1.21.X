2f7da4ac5cc81a60c088ec3df096e3b9d457c34b
Gas2g <gunser15gr@hotmail.com>
2024-09-15 20:13:28 +0300

    Initial commit

---
+++ src/main/java/name/startingmod/MyFirstMod.java
@@ -0,0 +1,16 @@
+package name.startingmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MyFirstMod implements ModInitializer {
+	public static final String MOD_ID = "startingmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/name/startingmod/MyFirstModClient.java
@@ -0,0 +1,10 @@
+package name.startingmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
